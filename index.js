79544860c51c642b9043964d701b7d752c64e336
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-02 19:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+  return "FUNKY"
+  
 }
+return  funkyFunction()
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
